cf34408f32e4da189b620122391f14134c3a4329
Mounir <polomnr1@gmail.com>
2024-12-21 22:11:55 +0100

    added shadcn

--- src/app/_components/LoginButton.tsx
+++ src/app/_components/LoginButton.tsx
@@ -1,7 +1,19 @@
 "use client";
 
 import { login } from "@/actions/actions";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
 
 export default function LoginButton() {
-  return <button onClick={() => login()}>Sign in</button>;
+  const [isPending, startTransition] = useTransition();
+
+  return (
+    <Button
+      variant="link"
+      onClick={() => startTransition(() => login())}
+      disabled={isPending}
+    >
+      Sign in
+    </Button>
+  );
 }
